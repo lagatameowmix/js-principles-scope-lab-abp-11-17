04334d7afd1411e3523bc8528ec8f028ad0b29c2
Sasha V <sashav4@gmail.com>
2017-11-12 05:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,4 @@ function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = 'harry';
 }
 let favoriteCustomer = 'carol';
-function changeFavoriteCustomer() {
-  favoriteCustomer = 'andrew';
-}
+let favoriteCustomer = 'susan'
